349cc14dec819e73f75bd9ad2648a1b49218be0e
David Bautista <wyrdaargetlam@gmail.com>
2014-08-16 18:07:23 +0200

    Añadida estructura para discusiones

--- bee/urls/trans_urls.py
+++ bee/urls/trans_urls.py
@@ -32,14 +32,18 @@ urlpatterns = patterns('',
     url(r'^project/(?P<proj_id>\d+)/niko_calendar/$', 'bee.views.scrum_projects.niko_calendar', name='niko_calendar'),
     url(r'^project/(?P<proj_id>\d+)/admin_project/$', 'bee.views.scrum_projects.admin_project', name='admin_project'),
     url(r'^project/(?P<proj_id>\d+)/add_participant_to_project/$', 'bee.views.scrum_projects.add_participant_to_project', name='add_participant_to_project'),
+    url(r'^project/(?P<proj_id>\d+)/discussions/$', 'bee.views.discussions.project_discussions', name='project_discussions'),
 
     url(r'^project/(?P<proj_id>\d+)/sprints/$', 'bee.views.scrum_projects.sprints', name='sprints'),
     url(r'^project/(?P<proj_id>\d+)/sprints/(?P<spr_id>\d+)/$', 'bee.views.scrum_sprints.sprint', name='sprint_overview'),
+    url(r'^project/(?P<proj_id>\d+)/sprints/(?P<spr_id>\d+)/discussions/$', 'bee.views.discussions.sprint_discussions', name='sprint_discussions'),
 
     url(r'^project/(?P<proj_id>\d+)/sprints/(?P<spr_id>\d+)/tasks/$', 'bee.views.scrum_sprints.tasks', name='tasks'),
     url(r'^project/(?P<proj_id>\d+)/sprints/(?P<spr_id>\d+)/create_task_colorbox/$', 'bee.views.scrum_sprints.create_task_colorbox', name='create_task_colorbox'),
     url(r'^project/(?P<proj_id>\d+)/sprints/(?P<spr_id>\d+)/create_task_js/$', 'bee.views.scrum_sprints.create_task_js', name='create_task_js'),
 
+
+    url(r'^project/(?P<proj_id>\d+)/sprints/(?P<spr_id>\d+)/task/(?P<task_id>\d+)/discussions/$', 'bee.views.discussions.task_discussions', name='task_discussions'),
 
     #SPRINTS & TASKS
 
